b346708fb7fb87980cdcee0cec3d3c6fc29c482a
vlazybones <eylinbxy@gmail.com>
2024-09-24 13:49:24 +0800

    edit response json

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -6,11 +6,14 @@ const authUsers = (req, res, next) => {
     token = req.headers.authorization.split(" ")[1];
   }
   if (!token) {
-    res.status(401).json("Unauthorized, login first");
+    // user not logged in
+    res.status(401).json({
+      success: false,
+      message: "Unauthorized, login first",
+    });
   } else {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.id;
-    // req.status(200);
   }
   next();
 };
@@ -27,12 +30,22 @@ const authGroups = (...users) => {
       const userPermittedGroups = groups.filter((group) => users.includes(group.groupname));
 
       if (userPermittedGroups.length === 0) {
-        res.status(401).json("Unauthorized access");
+        //not permitted to access
+        res.status(401).json({
+          success: false,
+          message: "Unauthorized access",
+        });
       } else {
-        res.status(200).json("Granted access");
+        res.status(200).json({
+          success: true,
+          message: "Granted access",
+        });
       }
     } catch (error) {
-      res.status(400).json(error.message);
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
     }
   };
 };
