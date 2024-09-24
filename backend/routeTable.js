af780df2594722864db8bab6cfe700e1bb8dc1e9
vlazybones <eylinbxy@gmail.com>
2024-09-24 13:59:25 +0800

    remove unused import

--- backend/routeTable.js
+++ backend/routeTable.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const loginController = require("./controllers/login");
-const authController = require("./controllers/auth");
 const { authUsers, authGroups } = require("./controllers/auth");
 router.post("/login", loginController);
 router.get("/TMS", authUsers, authGroups("Admin"));
