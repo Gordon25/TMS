e1067ce13fbe950a770beae2626371bc8e1f8155
vlazybones <eylinbxy@gmail.com>
2024-09-25 17:50:33 +0800

    resolve conflict

---
+++ backend/controllers/createGroup.js
@@ -0,0 +1,20 @@
+const asyncConnection = require("../db");
+module.exports = async (req, res) => {
+  const { group } = req.body;
+  try {
+    const connection = await asyncConnection;
+    await connection.query(
+      `INSERT INTO user_groups (groupname, username)
+      VALUES (${group}, NULL);`
+    );
+    res.status(200).json({
+      success: true,
+      message: "Group created successfully.",
+    });
+  } catch (error) {
+    res.status(error.status).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
