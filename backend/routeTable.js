06e30523d13bbc2d764f2244d6695b128158326e
vlazybones <eylinbxy@gmail.com>
2024-09-25 10:53:31 +0800

    Add note

--- backend/routeTable.js
+++ backend/routeTable.js
@@ -5,6 +5,6 @@ const { authUsers, authGroups } = require("./controllers/auth");
 const createUserController = require("./controllers/createUser");
 const assignGroupsController = require("./controllers/assignGroups");
 router.post("/login", loginController);
-router.get("/TMS", authUsers, authGroups("Admin"));
+router.get("/TMS", authUsers, authGroups("Admin")); //add middleware to render TMS page
 router.post("/createUser", authUsers, authGroups("Admin"), createUserController);
 module.exports = router;
