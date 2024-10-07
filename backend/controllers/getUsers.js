0537e0c0b0a4eaeb9557f159bfca23ad8b6b94a8
vlazybones <eylinbxy@gmail.com>
2024-10-07 09:24:07 +0800

    Update logic from removing id from accounts table

--- backend/controllers/getUsers.js
+++ backend/controllers/getUsers.js
@@ -2,7 +2,7 @@ import connection from "../utils/dbconnection.js";
 export default async (req, res) => {
   try {
     const [users, userFields] = await connection.query(
-      `SELECT id, username, email, isActive FROM accounts where isActive=1;`
+      `SELECT username, email, isActive FROM accounts where isActive=1;`
     );
     const [groups, groupFields] = await connection.query(
       `SELECT username, groupname FROM user_groups;`
