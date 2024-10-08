0ab1a412d593870b4b637c9aaf5118fbd8d83cc3
vlazybones <eylinbxy@gmail.com>
2024-10-08 14:21:02 +0800

    Fix logout logic

--- frontend/src/lib/logout.ts
+++ frontend/src/lib/logout.ts
@@ -1,24 +1,23 @@
 import axiosInstance from "./axiosConfig";
 import loginStatus from "./stores/loginStatus";
-import { redirect } from "@sveltejs/kit";
+
 const logout = async () => {
+  let success = false;
+  let message = "";
   try {
-    let success: boolean = true;
-    let message = "";
-    const responseData = await axiosInstance
-      .get(`/logout`)
-      .then((res) => res.data)
-      .catch((err) => err.response.data);
+    const responseData = await axiosInstance.get(`/logout`).then((res) => res.data);
+
     success = responseData.success;
     message = responseData.message;
     if (success) {
       loginStatus.update((status) => ({ ...status, isLoggedIn: false, isAdmin: false }));
-      return { success, message: "successfully logged out" };
+      console.log("LOGOUT RESPONSE ", responseData);
+      window.location.href = "/login";
     } else {
-      return { success, message };
+      return alert(message);
     }
   } catch (error) {
-    return { success: false, message: "Error logging out" };
+    console.log(error);
   }
 };
 export default logout;
