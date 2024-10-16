2d1519610c593dfd5b0c2b8c72f461994b65fa00
vlazybones <eylinbxy@gmail.com>
2024-10-16 09:09:58 +0800

    Update plan name validation logic"

--- backend/controllers/validatePlanName.js
+++ backend/controllers/validatePlanName.js
@@ -1,3 +1,4 @@
+import { connection } from "../utils/dbconnection.js";
 export default async (req, res, next) => {
   const field = "plan name";
   const planNameRegex = new RegExp("^(?=.*[a-zA-Z])[a-zA-Z0-9]+$");
@@ -10,6 +11,27 @@ export default async (req, res, next) => {
       message: "Plan name must be alphanumeric.",
     });
   } else {
-    next();
+    try {
+      const [matchedPlanNames, fields] = await connection.query(
+        "select plan_mvp_name from plans where plan_mvp_name=?;",
+        planName
+      );
+      console.log(matchedPlanNames);
+      if (matchedPlanNames.length != 0) {
+        res.status(200).json({
+          success: false,
+          field,
+          message: "Duplicate plan names not allowed.",
+        });
+      } else {
+        next();
+      }
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        field,
+        message: "Internal Server Errror.",
+      });
+    }
   }
 };
