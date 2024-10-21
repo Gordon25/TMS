5df351f3eaf9f5e6ae0cec9e2b8640680c17f803
vlazybones <eylinbxy@gmail.com>
2024-10-21 17:41:14 +0800

    Update logic in validate groups

--- backend/middleware/validateGroups.js
+++ backend/middleware/validateGroups.js
@@ -1,34 +1,34 @@
 export default (req, res, next) => {
   const { create, open, todo, doing, done } = req.body;
-  if (create === "") {
+  if (!create) {
     res.status(200).json({
       success: false,
       field: "create",
-      message: "Create Group cannot be null.",
+      message: "Create Group cannot be empty.",
     });
-  } else if (open === "") {
+  } else if (!open) {
     res.status(200).json({
       success: false,
       field: "open",
-      message: "Open Group cannot be null.",
+      message: "Open Group cannot be empty.",
     });
-  } else if (todo === "") {
+  } else if (!todo) {
     res.status(200).json({
       success: false,
       field: "todo",
-      message: "Todo Group cannot be null.",
+      message: "Todo Group cannot be empty.",
     });
-  } else if (doing === "") {
+  } else if (!doing) {
     res.status(200).json({
       success: false,
       field: "doing",
-      message: "Doing Group cannot be null.",
+      message: "Doing Group cannot be empty.",
     });
-  } else if (done === "") {
+  } else if (!done) {
     res.status(200).json({
       success: false,
       field: "done",
-      message: "Done Group cannot be null.",
+      message: "Done Group cannot be empty.",
     });
   } else {
     next();
