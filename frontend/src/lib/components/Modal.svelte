327c077cc1d7353a1b0284f0b3b09306defac38f
vlazybones <eylinbxy@gmail.com>
2024-10-22 10:43:22 +0800

    Fix Modal css

--- frontend/src/lib/components/Modal.svelte
+++ frontend/src/lib/components/Modal.svelte
@@ -1,12 +1,11 @@
 <script lang='ts'>
+	import { createEventDispatcher } from "svelte";
 	export let showModal:boolean;
   export let closeModal:()=>void;
 	let dialog:HTMLDialogElement; // HTMLDialogElement
 	let width:'1px';
 	let height:'1px';
-	let viewBoxValue= `0 0 ${height} ${width}`
-	
-	const svgPath="src/public/assets/icons/close.svg"
+	let dispatch = createEventDispatcher();
 	$: if (dialog && showModal) dialog.showModal();
 
 </script>
@@ -19,9 +18,7 @@
 	<!-- svelte-ignore a11y-no-static-element-interactions -->
 	<div class='placeholder' on:click|stopPropagation>
 		<!-- svelte-ignore a11y-autofocus -->
-		<button autofocus on:click={() => dialog.close()}>
-			<img src={svgPath} alt='Close button' class='close'/>
-		</button>
+		<button autofocus on:click={() => {dialog.close(); dispatch('closeModal');}}>X</button>
 		<slot/>
 		
 	</div>
@@ -34,7 +31,8 @@
 		border: none;
 		padding: 0;
     width:auto;
-    /* height:85%; */
+    /* height:90%; */
+		overflow-y: hidden;
 	}
 	dialog::backdrop {
 		background: rgba(0, 0, 0, 0.3);
@@ -44,7 +42,6 @@
 		padding: 0 1em;
 		align-items: center;
 		justify-items: center;
-		/* height: 100%; */
 	}
 	dialog[open] {
 		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
@@ -71,19 +68,16 @@
 	button {
 		display: block;
 		background-color: transparent;
-		width:1%;
-		height:1%;
+		margin-left: 98%;
+		width:2.5%;
+		height:30px;
 		position: relative;
-		left:98%;
 		top:0%;
 		justify-content: center;
 		align-items: center;
-	
+		border: transparent;
+		font-size: 20px;
 	}
-	.close {
-		height: 10px;
-		width:10px;
-		left:0;
-	}
+
 
 </style>
