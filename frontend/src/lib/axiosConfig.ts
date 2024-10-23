226e7176234c9e9ed2244588cb3099001256fa44
vlazybones <eylinbxy@gmail.com>
2024-10-23 18:16:10 +0800

    Add axiosCOnfig

--- frontend/src/lib/axiosConfig.ts
+++ frontend/src/lib/axiosConfig.ts
@@ -10,33 +10,22 @@ loginStatus.subscribe((status) => (isLoggedIn = status.isLoggedIn));
 const axiosInstance: Axios = axios.create({
   baseURL: PUBLIC_BACKEND_HOSTNAME, // Your backend API URL
   timeout: 10000, // Optional: Set a timeout
-  headers: {
-    // Add any custom headers here, e.g. Authorization tokens
-  },
   withCredentials: true,
 });
 
-// axiosInstance.interceptors.response.use(
-//   (response) => response, // Return the response for successful requests
-//   async (error: AxiosError) => {
-//     const url = error.response?.config.url; // Get the request URL
-//     const status = error.response?.status; // Get the status code
+axiosInstance.interceptors.response.use(
+  (response) => response, // Return the response for successful requests
+  async (error: AxiosError) => {
+    const url = error.response?.config.url; // Get the request URL
+    const status = error.response?.status; // Get the status code
 
-//     if (status === 403) {
-//       throw redirect(301, "/tms");
-//     } else if (status === 401) {
-//       if (url === "/login") {
-//         return Promise.reject(error.response); // Reject if already at the login page
-//       }
-//       console.log("ERROR STATUS", error.code, "MESSAGE", error.message, "URL ", url);
-//       if (isLoggedIn) {
-//         console.log("LOGGING USER OUT!");
-//         await logout(); // Call your logout function
-//       }
-//       console.log("USER IS LOGGED OUT, sending error ");
-//       return Promise.resolve(error.response);
-//     }
-//   }
-// );
+    if (status === 403) {
+      loginStatus.set({ isLoggedIn: true, isAdmin: false });
+      throw redirect(301, "/tms");
+    } else {
+      return Promise.reject(error); // Reject if already at the login page
+    }
+  }
+);
 
 export default axiosInstance;
